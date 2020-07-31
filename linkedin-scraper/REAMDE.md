43566f0cefdb426173229f34b616832fc51976ad
181192 <k@kalli.no>
2020-07-31 15:32:44 +0200

    Updates

--- linkedin-scraper/REAMDE.md
+++
@@ -1,5 +0,0 @@
-# Linkedin-scraper
-
-LinkedIn profile scraper using Puppeteer headless browser. So you can use it on a server. Returns structured data in JSON format.
-
-https://github.com/jvandenaardweg/linkedin-profile-scraper
--- web/src/types/index.js
+++
@@ -1,5 +0,0 @@
-export { arrayOf, shape } from "prop-types"
-export { ProfileType } from "./profile"
-export { ProjectType } from "./project"
-export { SocialType } from "./social"
-export { WorkHistoryType } from "./work-history"
