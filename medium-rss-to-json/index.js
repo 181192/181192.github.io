5e9760b45e98e54d1bc98b48f5e9817c694f58d5
181192 <k@kalli.no>
2020-07-31 12:38:27 +0200

    Filter out posts and add the author to the post object
    Add full url to post

--- medium-rss-to-json/index.js
+++ medium-rss-to-json/index.js
@@ -5,18 +5,32 @@ const axios = require("axios")
 const MEDIUM_URL = `https://medium.com/${process.env.MEDIUM_USERNAME}/has-recommended?format=json`
 
 async function getHasRecommended() {
-  let result = ""
+  let result
 
   try {
     const response = await axios.get(MEDIUM_URL)
 
-    let i = response.data.indexOf("{")
-    result = response.data.substr(i)
+    const i = response.data.indexOf("{")
+    const tmp = response.data.substr(i)
+    result = await JSON.parse(tmp)
   } catch (error) {
     console.error(error)
   }
 
-  const data = JSON.stringify(JSON.parse(result), null, 4)
+  posts = result.payload.references.Post
+  posts = Object.values(posts)
+
+  users = result.payload.references.User
+  posts = posts.map(post => {
+    let author = users[post.creatorId]
+    let props = {
+      author,
+      url: `https://medium.com/@${author.username}/${post.uniqueSlug}`,
+    }
+    return { ...post, ...props }
+  })
+
+  const data = JSON.stringify(posts, null, 4)
   const basePath = process.env.MEDIUM_BASE_PATH || "./build"
 
   try {
