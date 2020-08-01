34bd5bd46cd4ba05d9dc4c8d4d5d8df930746e7e
181192 <k@kalli.no>
2020-08-01 17:10:39 +0200

    Add github icon on starred repositories

--- web/src/components/starred-repositories/starred-repository.js
+++ web/src/components/starred-repositories/starred-repository.js
@@ -1,11 +1,12 @@
 import React from "react"
 import { StarredRepositoryType } from "../../types"
+import { FaGithub } from "react-icons/fa"
 
 const StarredRepository = props => {
   const { nameWithOwner, description, url } = props
 
   return (
-    <div className="border-t-4 border-line bg-back-light p-4 lg:p-8 bg-no-repeat text-sm mb-6">
+    <div className="border-t-4 border-line relative flex flex-wrap bg-back-light p-4 lg:p-8 bg-no-repeat text-sm mb-6">
       <div className="lg:flex-1">
         <h4 className="font-bold">{nameWithOwner}</h4>
         {url && (
@@ -19,6 +20,9 @@ const StarredRepository = props => {
           </a>
         )}
         <p className="w-full py-4 whitespace-pre-line">{description}</p>
+        <span className="absolute right-0 bottom-0 mb-5 mr-5 text-back">
+          <FaGithub className="w-6 h-6" />
+        </span>
       </div>
     </div>
   )
