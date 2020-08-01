ee2251b2f0a8c8b5ce10b9f82d622c05c3b51f80
181192 <k@kalli.no>
2020-08-01 17:25:20 +0200

    Render profile image from external source
    Add grayscale on image

--- web/src/components/sidebar/profile-image.js
+++ web/src/components/sidebar/profile-image.js
@@ -1,15 +1,15 @@
-import Img from "gatsby-image"
 import React from "react"
 import { ProfileType } from "../../types"
 
 const ProfileImage = ({ image, name }) => (
   <div className="relative inline-block leading-none">
-    <Img
-      fixed={image}
+    <img
+      src={image}
       alt={name}
       className="rounded-full relative z-10"
+      style={{ filter: "grayscale(90%)" }}
     />
-    <div className="absolute inset-0 rounded-full bg-lead z-0 transform translate-x-12"></div>
+    <div className="absolute inset-0 rounded-full bg-lead z-0 transform translate-x-8"></div>
   </div>
 )
 
