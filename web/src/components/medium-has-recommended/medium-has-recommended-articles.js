374413894b8d39681421c5f5e856721bac8975db
181192 <k@kalli.no>
2020-08-02 14:21:26 +0200

    Add link to medium profile

--- web/src/components/medium-has-recommended/medium-has-recommended-articles.js
+++ web/src/components/medium-has-recommended/medium-has-recommended-articles.js
@@ -1,19 +1,52 @@
 import React from "react"
 import MediumHasRecommended from "./medium-has-recommended"
 import { arrayOf, shape, MediumHasRecommendedType } from "../../types"
+import { graphql, useStaticQuery } from "gatsby"
 
-const MediumHasRecommendedArticles = ({ mediumHasRecommendedArticles }) => (
-  <>
-    <h5 className="font-header font-semibold text-front text-sm uppercase mb-3">
-      My latest recommendations from <a className="underline" href="https://medium.com">medium.com</a>
-    </h5>
-    <div className="flex-wrap">
-    {mediumHasRecommendedArticles.map((article, i) => (
-      <MediumHasRecommended key={`${article.name}_${i}`} {...article} />
-    ))}
-    </div>
-  </>
-)
+const MediumHasRecommendedArticles = ({ mediumHasRecommendedArticles }) => {
+  const { url: mediumUrl } = useStaticQuery(graphql`
+    query {
+      socialYaml(name: { eq: "medium" }) {
+        name
+        url
+      }
+    }
+  `).socialYaml
+
+  return (
+    <>
+      <h5 className="font-header font-semibold text-front text-sm uppercase mb-3">
+        My latest recommendations from{" "}
+        <a
+          className="underline"
+          href="https://medium.com"
+          alt="medium.com"
+          rel="noreferrer noopener"
+          target="_blank"
+        >
+          medium.com
+        </a>
+      </h5>
+      <div className="flex-wrap">
+        {mediumHasRecommendedArticles.map((article, i) => (
+          <MediumHasRecommended key={`${article.name}_${i}`} {...article} />
+        ))}
+      </div>
+      {mediumUrl && (
+        <p>
+          See more at{" "}
+          <a
+            className="underline"
+            href={`${mediumUrl}/has-recommended`}
+            alt="medium.com profile"
+            rel="noreferrer noopener"
+            target="_blank"
+          >{`${mediumUrl}/has-recommended`}</a>
+        </p>
+      )}
+    </>
+  )
+}
 
 MediumHasRecommendedArticles.propTypes = {
   mediumHasRecommendedArticles: arrayOf(shape(MediumHasRecommendedType)),
